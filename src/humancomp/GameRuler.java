b0f560ea1fbe9475ee61e0991756aeab1b6d9b61
s1242020 <s1242020@CORINNIS>
2016-11-04 16:11:50 +0100

    HumanComputitions

---
+++ src/humancomp/GameRuler.java
@@ -0,0 +1,7 @@
+package humancomp;
+
+public class GameRuler {
+	
+	
+
+}
---
+++ src/humancomp/RunnerNamingGame.java
@@ -0,0 +1,10 @@
+package humancomp;
+
+public class RunnerNamingGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
