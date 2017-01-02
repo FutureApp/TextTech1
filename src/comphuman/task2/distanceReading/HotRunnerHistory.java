a8e68e453f872ff15011362b0d4ba806012a4c02
Michael Czaja <michael-czaja-arbeit@hotmail.de>
2017-01-02 19:44:21 +0100

    small changes

---
+++ src/comphuman/task2/distanceReading/HotRunnerHistory.java
@@ -0,0 +1,9 @@
+package comphuman.task2.distanceReading;
+
+public class HotRunnerHistory {
+
+	/*
+	 * https://de.wikipedia.org/w/index.php?title=Diskussion:Liste_von_Hallo-Welt-Programmen/
+H%C3%B6here_Programmiersprachen&offset=&limit=2000&action=history
+	 */
+}
