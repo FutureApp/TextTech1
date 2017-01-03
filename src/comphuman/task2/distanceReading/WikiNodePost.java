61f08b49ac7388ff63d2ebba37ffccc8837d71f0
Michael Czaja <MichaelCzaja@fico.com>
2017-01-03 13:56:50 +0100

    Javadoc

--- src/comphuman/task2/distanceReading/WikiNodePost.java
+++ src/comphuman/task2/distanceReading/WikiNodePost.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 
 /**
- * Represents a post in Wikipedia 
+ * Represents a post from the wikipedia article-discussion section. Each post/node gets a unique ID. This is most important for the visualization-step. 
  * @author mcz
  *
  */
@@ -17,6 +17,14 @@ public class WikiNodePost {
 	String contentCreatedDate;
 	
 	ArrayList<WikiNodePost> wikiNodes;
+	
+	/**
+	 * Inits the wiki-post. A unique ID of this node will be generated automatically.
+	 * @param fatherNodeName Unique id from the father-node.
+	 * @param content Content of this node.
+	 * @param contentCreatedUsername The name of the user, which wrote this post.
+	 * @param contentCreatedDate The date of creation.
+	 */
 	public WikiNodePost(String fatherNodeName, String content, String contentCreatedUsername, String contentCreatedDate) {
 		super();
 		this.fatherNodeName = fatherNodeName;
@@ -25,6 +33,15 @@ public class WikiNodePost {
 		this.contentCreatedDate = contentCreatedDate;
 		this.nodeName = UUID.randomUUID().toString();
 	}
+	
+	/**
+	 * Inits the wiki-post.
+	 * @param UUID Unique ID for the node. Hint: Use UUID.randome()+"" for this purpose.
+	 * @param fatherNodeName Unique id from the father-node.
+	 * @param content Content of this node
+	 * @param contentCreatedUsername The name of the user, which wrote this post.
+	 * @param contentCreatedDate The date of creation.
+	 */
 	public WikiNodePost(String UUID,String fatherNodeName, String content, String contentCreatedUsername, String contentCreatedDate) {
 		super();
 		this.fatherNodeName = fatherNodeName;
@@ -33,24 +50,41 @@ public class WikiNodePost {
 		this.contentCreatedDate = contentCreatedDate;
 		this.nodeName = UUID;
 	}
+	
+	/**
+	 * Returns the ID of the father-node.
+	 * @return The ID of the father-node. 
+	 */
 	public String getFatherNodeName() {
 		return fatherNodeName;
 	}
+	/**
+	 * Returns the name of this node
+	 * @return Returns the ID of this node.
+	 */
 	public String getNodeName() {
 		return nodeName;
 	}
+	/**
+	 * Returns the content of this node.
+	 * @return Returns the content of this node.
+	 */
 	public String getContent() {
 		return content;
 	}
+	/**
+	 * Returns the user, which created for the corresponding post.
+	 * @return Returns the name of the user.
+	 */
 	public String getContentCreatedUsername() {
 		return contentCreatedUsername;
 	}
+	/**
+	 * Returns the date of creation for the corresponding post.
+	 * @return Returns the date of creation.
+	 */
 	public String getContentCreatedDate() {
 		return contentCreatedDate;
-	}
-	
-	public void hot() {
-		System.out.println();
 	}
 
 }
