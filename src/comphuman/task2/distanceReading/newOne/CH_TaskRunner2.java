26146680258c07837ba5e1a98a51f44f0ba6e772
mcz <mcz@NBMCZ.tonbeller.com>
2017-01-05 07:32:58 +0100

    javadoc and node-save method

--- src/comphuman/task2/distanceReading/newOne/CH_TaskRunner2.java
+++ src/comphuman/task2/distanceReading/newOne/CH_TaskRunner2.java
@@ -15,6 +15,8 @@ public class CH_TaskRunner2 {
 	static String encoding = Encoding.getDefaultEncoding();
 
 	static String resultDir = "CompHuman/result";
+	static String locationOfArticleNodes = "/article/nodeInformations.txt";
+	static String locationOfArticleVis = "/article/vis.txt";
 
 	/**
 	 * Entry-point of application. TODO
@@ -25,29 +27,80 @@ public class CH_TaskRunner2 {
 		arg = args;
 		validateAmountOfGivenInput();
 		cleanResultDir(resultDir);
-		ArrayList<ArrayList<Node>> resultAnalysis = runAnalysis(arg[0]);
+		ArrayList<ArrayList<Node>> resultAnalysis = runDisAnalysis(arg[0]);
 		vizAnalyseResults(resultAnalysis);
+		saveNodeInformations(resultAnalysis);
+		saveNodeAnalyse(resultAnalysis);
 
 	}
 
-	private static ArrayList<ArrayList<Node>> runAnalysis(String URL) {
+	/**
+	 * Saves all node information to a file. File is located under
+	 * ./article/nodeInformations.txt.
+	 * 
+	 * @param resultAnalysis
+	 *            Content to save(List of List of Nodes).
+	 */
+	private static void saveNodeInformations(ArrayList<ArrayList<Node>> resultAnalysis) {
+		File saveContentTo = new File(resultDir + locationOfArticleNodes);
+		String headerStructure = "Name of node | Name of father node | Created by User(user-name) | Creation Date";
+		if (saveContentTo.exists())
+			FileUtils.deleteQuietly(saveContentTo);
+		saveContentTo.mkdirs();
+		try {
+			FileUtils.write(saveContentTo, headerStructure, encoding, false);
+			for (int i = 0; i < resultAnalysis.size(); i++) {
+				ArrayList<Node> section = resultAnalysis.get(i);
+				FileUtils.write(saveContentTo, System.lineSeparator(), encoding, true);
+				for (Node node : section) {
+					String nodeName = node.getName();
+					String fatherName = node.getFather();
+					String userName = node.getAut();
+					String creationDate = node.getDate();
+					String statement = String.format("%d|%d|%d|%d|%d|", nodeName, fatherName, userName, creationDate);
+					FileUtils.write(saveContentTo, statement + System.lineSeparator(), encoding, true);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * @param resultAnalysis
+	 */
+	private static void saveNodeAnalyse(ArrayList<ArrayList<Node>> resultAnalysis) {
+
+	}
+
+	/**
+	 * Analyzes a given german wiki-link to an article(analyze ofdiscussion-page).
+	 * @param URL URL to an article in the german wikipedia.
+	 * @return Returns the posts from the discussion-page as nodes.
+	 */
+	private static ArrayList<ArrayList<Node>> runDisAnalysis(String URL) {
 		WikiArticle wikiArticle = new WikiArticle(URL);
 		System.out.println(wikiArticle.getArticleName());
 		System.out.println(wikiArticle.getWikiURL());
 		wikiArticle.searchForContent();
 		wikiArticle.getWikiArticlePage();
 		ArrayList<String> SectionsFromDisPage = wikiArticle.searchAndSaveSectionsFromDisPage();
-		ArtNodeExtractor nodeExtractor = new ArtNodeExtractor(SectionsFromDisPage,wikiArticle.getArticleName());
+		ArtNodeExtractor nodeExtractor = new ArtNodeExtractor(SectionsFromDisPage, wikiArticle.getArticleName());
 		nodeExtractor.extractNodes();
 		return nodeExtractor.getAllNodesAllSections();
-		
+
 	}
-	
+
 	private static void vizAnalyseResults(ArrayList<ArrayList<Node>> resultAnalysis) {
 		VisRichArtDis vis = new VisRichArtDis(resultAnalysis);
 		vis.startVizRichDis();
 	}
 
+	/**
+	 * Cleans the location of  the result-dir.
+	 * @param dir Dir where to save the results.
+	 */
 	private static void cleanResultDir(String dir) {
 		File file = new File(dir);
 		if (!file.exists())
@@ -55,7 +108,6 @@ public class CH_TaskRunner2 {
 		try {
 			FileUtils.cleanDirectory(new File(dir));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
@@ -79,7 +131,7 @@ public class CH_TaskRunner2 {
 	}
 
 	/**
-	 * Prints usage
+	 * Prints the usage
 	 */
 	private static void usage() {
 		System.out.println("---------- Usage ----------");
