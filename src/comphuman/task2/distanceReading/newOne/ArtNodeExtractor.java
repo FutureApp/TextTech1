9496e712282d16c085c3412c2dbfb6a1179c5e15
Michael Czaja <michael-czaja-arbeit@hotmail.de>
2017-01-05 04:49:57 +0100

    del unneeded lines of code

--- src/comphuman/task2/distanceReading/newOne/ArtNodeExtractor.java
+++ src/comphuman/task2/distanceReading/newOne/ArtNodeExtractor.java
@@ -17,67 +17,26 @@ public class ArtNodeExtractor {
 	}
 
 	public void extractNodes() {
-		// First entry contains garbage.
-//		for (int i = 1; i < 5; i++) {
 		for (int i = 1; i < potNodes.size(); i++) {
 			String section = potNodes.get(i);
-			String disHeader = extractDisHeader(section);
-			extractNode(section);
+			String[] split = section.split("\n");
+			Fitter fit = new Fitter(split);
+			fit.trimDL();
+			fit.trimUL();
+
+			RichExtractor myEx = new RichExtractor(fit.getContent(), artName);
+			myEx.extract();
 		}
 	}
 
-	private void extractNode(String section) {
-		String[] split = section.split("\n");
-		Fitter fit = new Fitter(split);
-		fit.trimDL();
-		fit.trimUL();
-		
-		RichExtractor myEx = new RichExtractor(fit.getContent(),artName); 
-		myEx.extract();
-	}
-
-	private Integer detLevel(String line) {
-		return countLeft(line);
-	}
-
-	private String extractDisHeader(String section) {
-		String disHeader = Jsoup.parse(section).getElementsByClass("mw-headline").text();
-		return disHeader;
-	}
-
-	public void hot(String section) {
-		
-	}
-
 	public Integer countLeft(String con) {
-	Integer c =0;
-	for (int i = 0; i < con.length(); i++) {
-		if(con.charAt(i) == ' ') c++;
-		else break;
+		Integer c = 0;
+		for (int i = 0; i < con.length(); i++) {
+			if (con.charAt(i) == ' ')
+				c++;
+			else
+				break;
+		}
+		return c;
 	}
-	return c;
-	}
-	
-	/*
-
-	 String line = section.split("\n")[i];
-			Integer level = detLevel(line);
-			System.out.println(line);
-			System.out.println(level);
-			nodeContent += line;
-			if(level == activeLevel){
-				System.out.println("GUT");
-				if(ex.containsSomethigLikeCreationDate(line)) System.out.println(ex.findCreationDate(line));
-				if(ex.containsSomethingLikeUserName(line)) System.out.println(ex.findUserName(line));
-				
-			}
-			if(level > activeLevel){
-				System.out.println("Next");
-				activeLevel++;
-			}
-			if(level < activeLevel) {
-				System.out.println("prev");
-				activeLevel--;
-			}
-	 */
 }
